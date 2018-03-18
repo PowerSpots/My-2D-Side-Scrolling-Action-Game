fd53a7c168dbf9530864c6dd3c19d52aceded9bf
yangkai <yangkai92@gmail.com>
2018-03-18 10:36:12 +0800

    The core gameplay functionality is finished
    This is my 4th submit:
    The gnome is able to be killed by colliding with traps, and is able to collect the treasure. 
    The game is functionally complete now.

---
+++ Assets/Scripts/SignalOnTouch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class SignalOnTouch : MonoBehaviour
+{
+
+    // 当产生碰撞时要触发的事件
+    public UnityEvent onTouch;
+
+    public bool playAudioOnTouch = true;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        SendSignal(collider.gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        SendSignal(collision.gameObject);
+    }
+
+    // 如果接触到的对象标签是Player，调用事件
+    void SendSignal(GameObject objectThatHit)
+    {
+        if (objectThatHit.CompareTag("Player"))
+        {
+            if (playAudioOnTouch)
+            {
+                var audio = GetComponent<AudioSource>();
+
+                if (audio && audio.gameObject.activeInHierarchy)
+                    audio.Play();
+            }
+            onTouch.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteSwapper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSwapper : MonoBehaviour
+{
+    public Sprite spriteToUse;
+
+    // 要交换精灵图片的对象的精灵渲染器
+    public SpriteRenderer spriteRenderer;
+
+    // 原来的精灵图片，用来重设精灵图片
+    private Sprite originalSprite;
+
+    public void SwapSprite()
+    {
+
+        // 如果要使用的精灵图片和现在的精灵不同，发生交换
+        if (spriteToUse != spriteRenderer.sprite)
+        {
+            //保存初始的精灵图片
+            originalSprite = spriteRenderer.sprite;
+
+            spriteRenderer.sprite = spriteToUse;
+        }
+    }
+
+    // 重设会初始精灵图片
+    public void ResetSprite()
+    {
+
+        // 如果初始精灵图存在
+        if (originalSprite != null)
+        {
+            spriteRenderer.sprite = originalSprite;
+        }
+    }
+}

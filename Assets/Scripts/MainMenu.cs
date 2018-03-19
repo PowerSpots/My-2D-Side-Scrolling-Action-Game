c1e21a79d4c0f315b695a1b986bf43dddf2c6891
yangkai <yangkai92@gmail.com>
2018-03-19 10:52:51 +0800

    Basic game is done
    This is my 6th submit:
    The game level was built with main menu and main scene and can be played functionally.
    To polish more, a ghost prefab and traps, levels, and smoke and burning effects need to be added.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // 要加载的场景名称
+    public string sceneToLoad;
+
+    // 载入中 文本UI控件
+    public RectTransform loadingOverlay;
+
+    // 场景背景加载，用于控制场景切换的时间。
+    AsyncOperation sceneLoadingOperation;
+
+    public void Start()
+    {
+
+        // 禁用 载入中 文本UI
+        loadingOverlay.gameObject.SetActive(false);
+
+        // 开始在后台载入游戏主场景
+        sceneLoadingOperation =
+SceneManager.LoadSceneAsync(sceneToLoad);
+
+        // 直到主场景准备好之前，并不真正切换到主场景
+        sceneLoadingOperation.allowSceneActivation = false;
+
+
+    }
+
+    // 新游戏 按钮被点击时调用的载入场景方法
+    public void LoadScene()
+    {
+
+        loadingOverlay.gameObject.SetActive(true);
+
+        // 切换到已经准备好的主场景
+        sceneLoadingOperation.allowSceneActivation = true;
+
+    }
+
+
+}
+// END 2d_mainmenu
